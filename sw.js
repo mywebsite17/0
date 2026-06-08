80e482411b441615ad5449036a47462d79055d33
mywebsite17 <omarsalam2214+omar@gmail.com>
2026-06-08 21:02:53 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [ './index.html', './offline.html', './icon-192.png', './icon-512.png', './manifest.json' ];
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)).catch(() => caches.match('./offline.html')));
+});
