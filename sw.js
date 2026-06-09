7de6fe5aaf5460acdea2b4b43c7c835993131b9d
mywebsite17 <omarsalam2214+omar@gmail.com>
2026-06-09 16:24:40 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pwa-cache-v22';
+const CACHE_NAME = 'pwa-cache-v41';
 const urlsToCache = [ './', './index.html', './offline.html', './icon-192.png', './icon-512.png', './manifest.json' ];
 
 self.addEventListener('install', event => {
